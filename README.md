8e270899c43019eaca5adc3e0b30f9f37a25fa8b
Lance Ding <lance-ding@outlook.com>
2017-01-07 10:59:25 +0800

    创建基础服务

---
+++ README.md
@@ -0,0 +1,13 @@
+# MouseCtrl
+
+通过在提供的web服务页面里控制鼠标的操作(只控制左键点击)
+
+
+## Run
+
+`npm install && npm start`
+
+## 环境说明
+
+- 只在 Mac、Linux 机器测试运行
+- 使用了 RobotJS 包, 需要确定安装了GCC
---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen('8016', function(){
+  console.log('开始web服务: 8016...');
+});
+
