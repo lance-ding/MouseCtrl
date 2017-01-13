c51d479f0a0e2f79ca0213df4d0da5182a9d6859
Lance Ding <lance-ding@outlook.com>
2017-01-13 13:20:00 +0800

    配置socket.io

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+var path = require('path');
 var express = require('express');
 var app = express();
-var path = require('path');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen('8016', function(){
-  console.log('开始web服务: 8016...');
+http.listen('8016', function(){
+  console.log('已开始web服务, Port: 8016');
 });
 
+io.on('connection', function(socket){
+  console.log('已初始化 socket 连接');
+
+  socket.emit('ready', {some: 'data'});
+  socket.on('toServer', function(data){
+    console.log(data.some);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('已断开 socket 连接');
+  });
+});
