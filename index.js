dcdee67ed56b51814a8c279c0fc8cebe30f6382e
Lance Ding <lance-ding@outlook.com>
2017-01-16 00:39:07 +0800

    增加生成本机地址的二维码，可以通过手机扫描打开

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 var path = require('path');
+var os = require('os');
+var fs = require('fs');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var robot = require('robotjs');
+var qr = require('qr-image');
+var port = "8016";
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+app.get('/server', function(req, res){
+  // 获取当前内网IP
+  var interfaces = os.networkInterfaces();
+  console.log(interfaces);
+  var localIp;
+  for(var devName in interfaces){
+    var interface = interfaces[devName];
+    for(var i = 0; i < interface.length; i++){
+      var alias = interface[i];
+      if(interface[i].family == 'IPv4' 
+         && 
+         interface[i].address !== '127.0.0.1' 
+         && 
+        !interface[i].internal
+        &&
+        interface[i].address.substr(0, 7) == '192.168'){
+        localIp = interface[i].address;
+      }
+    }
+  };
+  var url = 'http://' + localIp + ':' + port;
+  // 生成二维码
+  var qrStream = qr.imageSync(url);
+  fs.writeFileSync(path.join(__dirname, 'public/images/local-ip.png'), qrStream);
+  res.sendFile(path.join(__dirname, 'views/server.html'));
 });
 
-http.listen('8016', function(){
+// 启动服务
+http.listen(port, function(){
   console.log('已开始web服务, Port: 8016');
 });
 
